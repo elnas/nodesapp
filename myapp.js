6429fc1a4d2f0535d4a9d05cea177a2e2eff9b88
elnas abdul <elnasiru@gmail.com>
2017-08-28 13:52:15 +0100

    Initial commit

---
+++ myapp.js
@@ -0,0 +1,22 @@
+var express  = require ('express');
+var app = express();
+var routes = require('./routes');
+
+app.set('view engine', 'ejs');
+app.locals.pagetitle = "Awesome Website ";
+
+app.get('/',routes.index);
+app.get('/about',routes.about);
+
+
+
+
+app.get('*', function(req, res) {
+	
+
+	res.send('Bad Route');//Routes are used tp create single page apps
+});
+
+var server = app.listen(3000, function() {
+	console.log('listening on port 3000');//express supports routing
+});
